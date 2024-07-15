38adf583ee8ed9919fce089286f26a35ae2331a5
mwhitlow83 <mwhitlow83@gmail.com>
2024-07-15 15:23:36 -0500

    process URL for fetch

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 
 
 //
+var str;
 
-
-
+// handle form submission
 function submitForm() {
     const form = document.getElementById('gogo');
     form.addEventListener('submit', (event) => {
@@ -14,16 +14,28 @@ function submitForm() {
       if (form.searchfield.value == "") {
         alert("Ensure you input a value in both fields!");
       }
-      console.log(form.searchfield.value);
-      
+     // console.log(typeof(form.searchfield.value));
+      const str = "http://api.giphy.com/v1/gifs/search?q=" + replaceSpacesWithPlus(form.searchfield.value)+"&api_key=wxr9aVNebIOeCh8xsW3ikOPeltf3PdjK&limit=5";
+      //console.log(str);
+
+      var result; fetch(str)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    
+    console.log(result);
+    
+
+
     });
 
-    form.submit();
+    form.submit();  
 
-    
+
   }
 
-
+function replaceSpacesWithPlus(garbage){
+    return garbage.replace(' ', '+');
+}
 
 
 
