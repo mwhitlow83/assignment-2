870fcea8a464e7ab67da173acd94ca337b6e2891
mwhitlow83 <mwhitlow83@gmail.com>
2024-07-19 08:16:50 -0500

    display number of gifs

--- main.js
+++ main.js
@@ -4,25 +4,37 @@
   function submitForm() {
       
    const sb = document.getElementById("sf").value;
-        
+   const lf = document.getElementById("lf").value;    
          
-        const str = "http://api.giphy.com/v1/gifs/search?q=" + replaceSpacesWithPlus(sb)+"&api_key=MY_KEY&limit=6";
-        
+        const str = "http://api.giphy.com/v1/gifs/search?q=" + replaceSpacesWithPlus(sb)+"&api_key=MY_KEY&limit="+lf;
+        console.log(str);
 
       //const str =  "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=MY_KEY&limit=6"     
-            // above is sample url from website 
+            // above is sample url from website CcrZiHoB9vpQJuHNpiSYlD1EAuVREs09takeoff
    
              fetch(str)
             .then((response) => response.json())
-            .then((result) => {  document.getElementById('box1').src=result.data[0]["embed_url"];
-                                 document.getElementById('box2').src=result.data[1]["embed_url"];
-                                 document.getElementById('box3').src=result.data[2]["embed_url"];
-                                 document.getElementById('box4').src=result.data[3]["embed_url"];
-                                 document.getElementById('box5').src=result.data[4]["embed_url"];
-                                 document.getElementById('box6').src=result.data[5]["embed_url"];                                       
+            .then((result) => {  
+
+              // variable lf is the limit of gifs that my fetch is sending back
+              for (let i = 0; i < lf; i++) {
+                
+                //i want to create an element for each gif
+                b = "box" + i.toString();
+                const para = document.createElement("iframe");    
+                const element = document.getElementById("square-container");
+                element.appendChild(para); 
+                para.setAttribute("id", b);
+                                           
+                document.getElementById(b).src=result.data[i]["embed_url"];
+                para.setAttribute("class","iframe" );
+                                                                                  
+              
+                                            }   //outside of for loop
+                                               
                               });
                     
-                            };
+                      }
 
     //function to replace spaces with + symbols
     function replaceSpacesWithPlus(garbage){
